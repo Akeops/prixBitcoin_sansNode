e452a2ae73ff128753dfde38ba111658c9027f46
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-05-19 09:52:20 +0200

    Ajout du cours en USD

--- script.js
+++ script.js
@@ -17,45 +17,9 @@ divElements.forEach(divElement => {
   });
 });
 
-
-/*function bitcoinTempsReelEUR(){
-  fetch('https://blockchain.info/ticker')
-  .then(response => response.json())
-  .then(data => {
-    const euroLast = data.EUR.last;
-    console.log(euroLast);
-    document.querySelector('#price_label_eur').textContent = euroLast;
-  })
-  .catch(error => {
-    console.error('Une erreur s\'est produite :', error);
-  });
-}
-
-function bitcoinTempsReelUSD(){
-  fetch('https://blockchain.info/ticker')
-  .then(response => response.json())
-  .then(data => {
-    const usdLast = data.USD.last;
-    console.log(usdLast);
-    document.querySelector('#price_label_usd').textContent = usdLast;
-  })
-  .catch(error => {
-    console.error('Une erreur s\'est produite :', error);
-  });
-}
-
-bitcoinTempsReelEUR();
-
-setInterval(bitcoinTempsReelEUR, 10000);
-  
-
-bitcoinTempsReelUSD();
-
-setInterval(bitcoinTempsReelUSD, 10000);*/
-
 const url = 'https://blockchain.info/ticker';
 
-function recupererPrix() {
+function recupererPrixEUR() {
   // Créer une requête
   let requete = new XMLHttpRequest(); // Créer un objet
   requete.open('GET', url); // Premier paramètre GET / POST, deuxième paramètr : url
@@ -77,5 +41,30 @@ function recupererPrix() {
   }
   console.log('Prix actualisé');
 }
-recupererPrix();
-setInterval(recupererPrix, 1000);
+recupererPrixEUR();
+setInterval(recupererPrixEUR, 1000);
+
+function recupererPrixUSD() {
+  // Créer une requête
+  let requete = new XMLHttpRequest(); // Créer un objet
+  requete.open('GET', url); // Premier paramètre GET / POST, deuxième paramètr : url
+  requete.responseType = 'json'; // Nous attendons du JSON
+  requete.send(); // Nous envoyons notre requête
+
+  // Dèss qu'on reçoit une réponse, cette fonction est executée
+  requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response; // on stock la réponse
+        let prixEnDollars = reponse.USD.last;
+        document.querySelector('#price_label_usd').textContent = prixEnDollars;
+      }
+      else {
+        alert('Un problème est intervenu, merci de revenir plus tard.');
+      }
+    }
+  }
+  console.log('Prix actualisé');
+}
+recupererPrixUSD();
+setInterval(recupererPrixUSD, 1000);
