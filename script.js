ff4a35a330d16574e00827ae2b46aa8950dbb92e
Ando_lalanne <lalanne.andoni1@gmail.com>
2023-05-23 11:34:22 +0200

    J'ai réussi à afficher le cours du bitcoin en 3 monnaies différentes. J'y ai mis un petit style javascript au passage de la souris.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function recupererPrixEUR() {
   requete.responseType = 'json'; // Nous attendons du JSON
   requete.send(); // Nous envoyons notre requête
 
-  // Dèss qu'on reçoit une réponse, cette fonction est executée
+  // Dès qu'on reçoit une réponse, cette fonction est executée
   requete.onload = function() {
     if (requete.readyState === XMLHttpRequest.DONE) {
       if (requete.status === 200) {
@@ -39,10 +39,10 @@ function recupererPrixEUR() {
       }
     }
   }
-  console.log('Prix actualisé');
+  console.log("allo");
 }
 recupererPrixEUR();
-setInterval(recupererPrixEUR, 1000);
+setInterval(recupererPrixEUR, 5000);
 
 function recupererPrixUSD() {
   // Créer une requête
@@ -64,7 +64,31 @@ function recupererPrixUSD() {
       }
     }
   }
-  console.log('Prix actualisé');
 }
 recupererPrixUSD();
-setInterval(recupererPrixUSD, 1000);
+setInterval(recupererPrixUSD, 5000);
+
+function recupererPrixGBP() {
+  // Créer une requête
+  let requete = new XMLHttpRequest(); // Créer un objet
+  requete.open('GET', url); // Premier paramètre GET / POST, deuxième paramètr : url
+  requete.responseType = 'json'; // Nous attendons du JSON
+  requete.send(); // Nous envoyons notre requête
+
+  // Dèss qu'on reçoit une réponse, cette fonction est executée
+  requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response; // on stock la réponse
+        let prixEnLivres = reponse.GBP.last;
+        document.querySelector('#price_label_gbp').textContent = prixEnLivres;
+      }
+      else {
+        alert('Un problème est intervenu, merci de revenir plus tard.');
+      }
+    }
+  }
+  console.log("prix actualisé");
+}
+recupererPrixGBP();
+setInterval(recupererPrixGBP, 5000);
